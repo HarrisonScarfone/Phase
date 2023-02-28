39060761bce40589e006cf87110e5eeded17d0b2
HarrisonScarfone <hscarfone77@gmail.com>
2023-02-27 22:43:45 -0500

    some experiments with move ordering

--- src/search/search.cpp
+++ src/search/search.cpp
@@ -1,26 +1,40 @@
 #include "search.hpp"
 
 #include <algorithm>
+#include <chrono>
+#include <iostream>
 #include <limits>
 #include <vector>
 
 #include "../evaluator/evaluator.hpp"
 #include "../game/moves.hpp"
 
+#define CHECKMATE_FOR_LAST_PLAYER -75000
+#define THEIR_BEST_MOVE_START_VAL 100000
+#define MY_BEST_MOVE_START_VAL -100000
+
+uint32_t find_move_with_statistics(Position* position, int depth)
+{
+  auto start_time = std::chrono::high_resolution_clock::now();
+  uint32_t best_move = find_move(position, depth);
+  auto end_time = std::chrono::high_resolution_clock::now();
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+  std::cout << "Time taken: " << duration.count() << "ms" << std::endl;
+
+  return best_move;
+}
+
 uint32_t find_move(Position* position, int depth)
 {
   uint32_t best_move;
-  int_fast32_t best_score = INT_FAST32_MIN;
+  int_fast32_t best_score = MY_BEST_MOVE_START_VAL;
   int_fast32_t current_score;
   std::vector<uint32_t> moves = valid_moves_for_position(*position);
-
-  int_fast32_t alpha = -100000;
-  int_fast32_t beta = 100000;
 
   for (uint32_t move : moves)
   {
     Position new_position = make_move(position, move);
-    current_score = -negamax(&new_position, depth - 1, -beta, -alpha);
+    current_score = -negamax(&new_position, depth - 1, -THEIR_BEST_MOVE_START_VAL, -MY_BEST_MOVE_START_VAL);
 
     if (current_score > best_score)
     {
@@ -32,30 +46,30 @@ uint32_t find_move(Position* position, int depth)
   return best_move;
 }
 
-int_fast32_t negamax(Position* position, int depth, int_fast32_t alpha, int_fast32_t beta)
+int_fast32_t negamax(Position* position, int depth, int_fast32_t my_best_move, int_fast32_t their_best_move)
 {
   if (depth == 0)
   {
     return evaluate_position(position);
   }
 
-  int_fast32_t score = -100000;
+  int_fast32_t score = MY_BEST_MOVE_START_VAL;
   std::vector<uint32_t> possible_moves = valid_moves_for_position(*position);
 
   if (possible_moves.empty())
   {
-    return -75000;
+    return CHECKMATE_FOR_LAST_PLAYER;
   }
 
   for (uint32_t move : possible_moves)
   {
     Position new_position = make_move(position, move);
-    int_fast32_t current_score = -negamax(&new_position, depth - 1, -beta, -alpha);
+    int_fast32_t current_score = -negamax(&new_position, depth - 1, -their_best_move, -my_best_move);
 
     score = std::max(current_score, score);
-    alpha = std::max(score, alpha);
+    my_best_move = std::max(score, my_best_move);
 
-    if (alpha >= beta)
+    if (my_best_move >= their_best_move)
     {
       break;
     }
@@ -63,3 +77,80 @@ int_fast32_t negamax(Position* position, int depth, int_fast32_t alpha, int_fast
 
   return score;
 }
+
+// uint32_t find_move(Position* position, int depth)
+// {
+//   uint32_t best_move;
+//   int_fast32_t best_score = MY_BEST_MOVE_START_VAL;
+//   int_fast32_t current_score;
+//   int_fast32_t look_ahead_score;
+//   std::vector<uint32_t> moves = valid_moves_for_position(*position);
+
+//   std::vector<std::tuple<uint32_t, int_fast32_t, Position>> scored_moves;
+//   for (uint32_t move : moves)
+//   {
+//     Position new_position = make_move(position, move);
+//     look_ahead_score = evaluate_position(&new_position);
+//     scored_moves.emplace_back(move, look_ahead_score, new_position);
+//   }
+
+//   std::sort(scored_moves.begin(), scored_moves.end(), [](auto& a, auto& b) { return std::get<1>(a) > std::get<1>(b);
+//   });
+
+//   for (std::tuple<uint32_t, int_fast32_t, Position> scored_move : scored_moves)
+//   {
+//     current_score = -negamax(&std::get<2>(scored_move), depth - 1, -THEIR_BEST_MOVE_START_VAL,
+//     -MY_BEST_MOVE_START_VAL);
+
+//     if (current_score > best_score)
+//     {
+//       best_move = std::get<0>(scored_move);
+//       best_score = current_score;
+//     }
+//   }
+
+//   return best_move;
+// }
+
+// int_fast32_t negamax(Position* position, int depth, int_fast32_t my_best_move, int_fast32_t their_best_move)
+// {
+//   if (depth == 0)
+//   {
+//     return evaluate_position(position);
+//   }
+
+//   int_fast32_t score = MY_BEST_MOVE_START_VAL;
+//   int_fast32_t look_ahead_score;
+//   std::vector<uint32_t> possible_moves = valid_moves_for_position(*position);
+
+//   if (possible_moves.empty())
+//   {
+//     return CHECKMATE_FOR_LAST_PLAYER;
+//   }
+
+//   std::vector<std::pair<int_fast32_t, Position>> scored_moves;
+//   for (uint32_t move : possible_moves)
+//   {
+//     Position new_position = make_move(position, move);
+//     look_ahead_score = evaluate_position(&new_position);
+//     scored_moves.emplace_back(look_ahead_score, new_position);
+//   }
+
+//   std::sort(scored_moves.begin(), scored_moves.end(), [](auto& a, auto& b) { return std::get<0>(a) > std::get<0>(b);
+//   });
+
+//   for (std::pair<int_fast32_t, Position> scored_move : scored_moves)
+//   {
+//     int_fast32_t current_score = -negamax(&std::get<1>(scored_move), depth - 1, -their_best_move, -my_best_move);
+
+//     score = std::max(current_score, score);
+//     my_best_move = std::max(score, my_best_move);
+
+//     if (my_best_move >= their_best_move)
+//     {
+//       break;
+//     }
+//   }
+
+//   return score;
+// }
--- src/search/search.hpp
+++ src/search/search.hpp
@@ -6,5 +6,6 @@
 // int minimax(Position* position, int depth, int_fast32_t alpha, int_fast32_t beta);
 int_fast32_t negamax(Position* position, int depth, int_fast32_t alpha, int_fast32_t beta);
 uint32_t find_move(Position* position, int depth);
+uint32_t find_move_with_statistics(Position* position, int depth);
 
 #endif
